95fbeb537f6331a53023b2f6570e0ce3d8a9fb24
Dendrary <almaz-nizamov2004@mail.ru>
2024-03-14 11:06:43 +0300

    commit

---
+++ NumberBetween.cs
@@ -0,0 +1,33 @@
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+string? readResult;
+string valueEntered = "";
+int numValue = 0;
+bool validNumber = false;
+
+do
+{
+	readResult = Console.ReadLine();
+	if (readResult != null)
+	{
+		valueEntered = readResult;
+	}
+	
+	validNumber = int.TryParse(valueEntered, out numValue);
+	
+	if (validNumber == true)
+	{
+		if(numValue < 5 || numValue > 10)
+		{
+			validNumber = false;
+			Console.WriteLine($"Your entered {numValue}. Please enter a number between 5 and 10.");
+		}
+	}
+	else 
+	{
+		Console.WriteLine("Sorry, your entered an invalid number, please try again.");
+	}
+
+} while (validNumber == false);
+
+Console.WriteLine($"Your input value ({numValue}) has been accepted");
